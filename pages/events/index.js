d5c1b47bf1d3430ab7bd201dd1a0f40d3466fb03
mathetes <nsk.roman@gmail.com>
2021-05-11 17:03:29 +0700

    folder 3 done

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,33 @@
-import Layout from "../../components/Layout";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
